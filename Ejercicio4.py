0239ba25efe1dbd9d1de6520fd0e86d1240b050a
Wilson Yaucán <75143213+wyaucan@users.noreply.github.com>
2022-12-28 10:57:15 -0500

    Delete Ejercicio4.py

--- Ejercicio4.py
+++
@@ -1,5 +0,0 @@
-#Escribir un programa que pida al usuario un número entero positivo y muestre por pantalla la cuenta atrás desde ese número hasta cero separados por comas.
-
-numero = int(input("Ingrese un numero: "))
-for i in range (numero + 1):
-    print (numero-i)
